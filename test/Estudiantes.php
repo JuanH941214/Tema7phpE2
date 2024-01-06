07c747f89c8bb7dd7d612c4ec31cffc24e4781e6
JuanH941214 <juanhernandez941214@gmail.com>
2024-01-06 19:16:06 +0100

    funcionando

--- test/Estudiantes.php
+++ test/Estudiantes.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
 class Estudiantes
 {
     public function obtenerGrado($int_nota)
     {
-        if ($int_nota >= 60 && $int_nota <= 100) {
+        if ($int_nota < 0 || $int_nota > 100) {
+            return "ingrese un valor valido";
+        } elseif ($int_nota >= 60 && $int_nota <= 100) {
             return "primera division";
         } elseif ($int_nota >= 45 && $int_nota <= 59) {
             return "segunda division";
@@ -14,4 +16,3 @@ class Estudiantes
         }
     }
 }
-?>
--- test/EstudiantesTest.php
+++ test/EstudiantesTest.php
@@ -3,15 +3,49 @@ use PHPUnit\Framework\TestCase;
 require "Estudiantes.php";
 class EstudiantesTest extends TestCase{
 
-    public function testObtenerGrado()
+    public function testValorNegativo()
+    {
+        $estudiantes = new Estudiantes();
+        $this->assertEquals("ingrese un valor valido", $estudiantes->obtenerGrado(-2));
+    }
+
+
+    public function testSuperiorLimite()
+    {
+        $estudiantes = new Estudiantes();
+        $this->assertEquals("ingrese un valor valido", $estudiantes->obtenerGrado(110));
+        
+    }
+
+    public function testObtenerPrimera()
     {
         $estudiantes = new Estudiantes();
         $this->assertEquals("primera division", $estudiantes->obtenerGrado(64));
-        $this->assertEquals("segunda division", $estudiantes->obtenerGrado(46));
-        $this->assertEquals("tercera division", $estudiantes->obtenerGrado(36));
-        $this->assertEquals("reprobaste", $estudiantes->obtenerGrado(24));
- 
     }
+
+
+    public function testObtenerSegunda()
+    {
+        $estudiantes = new Estudiantes();
+        $this->assertEquals("segunda division", $estudiantes->obtenerGrado(46));
+    }
+
+
+    public function testObtenerTercera()
+    {
+        $estudiantes = new Estudiantes();
+        $this->assertEquals("tercera division", $estudiantes->obtenerGrado(36));
+    }
+
+    public function testReprobar()
+    {
+        $estudiantes = new Estudiantes();
+        $this->assertEquals("reprobaste", $estudiantes->obtenerGrado(24));
+    }
+
+
+
+
     
 }
 
